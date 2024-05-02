02d1dbc17d22580b22bb01e7ae3599b4dfb13bb0
tin <minhtien0601200@gmail.com>
2024-05-03 00:08:31 +0700

    Add ChatController and modify LoginController

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,62 +1,112 @@
 package controllers;
 
+import com.example.member.Main;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-public class LoginController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    private HBox accountPart;
+import javafx.fxml.FXMLLoader;
 
-    @FXML
-    private ImageView attachBox;
-
-    @FXML
-    private HBox deadlinePart;
+public class Controller implements Initializable {
 
     @FXML
-    private ImageView emojiBox;
+    private Button btnClose;
+
 
     @FXML
-    private HBox insightPart;
+    private Button btnLogin;
 
     @FXML
-    private HBox messageBox;
+    private Label errorMessageLabel;
 
     @FXML
-    private TextField messageText;
+    private PasswordField pfPassword;
 
     @FXML
-    private ImageView moreBox;
+    private TextField tfUsername;
 
-    @FXML
-    private ImageView moreInfoBox;
+    private String errorMessage = "";
 
-    @FXML
-    private HBox overviewPart;
+    private boolean isFieldsFilled() {
+        errorMessage = "";
+        boolean isFilled = true;
 
-    @FXML
-    private ImageView phoneBox;
+        if (tfUsername.getText().isEmpty()) {
+            errorMessage = "Username is empty!";
+            isFilled = false;
+        }
+        if (pfPassword.getText().isEmpty()) {
+            if (!errorMessage.isEmpty()) {
+                errorMessage += "\n";
+            }
+            errorMessage += "Password is empty!";
+            isFilled = false;
+        }
 
-    @FXML
-    private TextField searchField;
+        errorMessageLabel.setText(errorMessage);
+        return isFilled;
+    }
 
-    @FXML
-    private TextField searchPartField;
+    private boolean isValid() {
+        errorMessage = "";
+        boolean isValid = true;
+        if (!tfUsername.getText().equals(Main.USERNAME)) {
+            errorMessage = "Invalid Username!";
+            isValid = false;
+        }
+        if (!pfPassword.getText().equals(Main.PASSWORD)) {
+            if (!errorMessage.isEmpty()) {
+                errorMessage += "\n";
+            }
+            errorMessage += "Invalid Password!";
+            isValid = false;
+        }
 
-    @FXML
-    private ImageView sendMessage;
+        errorMessageLabel.setText(errorMessage);
+        return isValid;
+    }
 
-    @FXML
-    private HBox settingPart;
+    private void startHomeWindow() {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Dashboard.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    @FXML
-    private HBox todoPart;
-
-    @FXML
-    private ImageView videoCallBox;
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btnClose.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.exit(0);
+            }
+        });
+        
+        btnLogin.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (isFieldsFilled() && isValid()) {
+                    startHomeWindow();
+                }
+            }
+        });
+    }
 }
-
