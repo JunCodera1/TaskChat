e940984e75c003636ca3725cf285964a53af307d
tin <minhtien0601200@gmail.com>
2024-05-06 14:20:01 +0700

    update TodoController

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,13 @@ module com.example.member {
     requires javafx.fxml;
     requires java.sql;
     requires mysql.connector.java;
+    requires org.eclipse.collections.api;
+    requires org.eclipse.collections.impl;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires jackson.datatype.eclipse.collections;
 
 
     opens com.example.member to javafx.fxml;
